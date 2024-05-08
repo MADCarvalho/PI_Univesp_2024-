4820afdd202109dbd683505cb81d6a25ec379aed
MADCarvalho <marco.delfino@hotmail.com>
2024-05-08 13:28:32 -0300

    atualizado app.py

--- app.py
+++ app.py
@@ -3,21 +3,19 @@ from auth import auth_bp
 from main import main_bp
 from extensions import db, init_app
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'projetointegradorunivesp2024'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:e5S6IVgfAFuY@ep-muddy-morning-a64mj7do-pooler.us-west-2.aws.neon.tech:5432/verceldb?sslmode=require"
 
-    init_app(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'projetointegradorunivesp2024'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:e5S6IVgfAFuY@ep-muddy-morning-a64mj7do-pooler.us-west-2.aws.neon.tech:5432/verceldb?sslmode=require"
 
-    with app.app_context():
+init_app(app)
+
+with app.app_context():
         db.create_all()
 
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(main_bp)
 
-    return app
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
